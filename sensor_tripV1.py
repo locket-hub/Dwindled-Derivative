82dee41826251351b5c05b6b13f8b769e0e13795
locket-hub <84401976+locket-hub@users.noreply.github.com>
2025-11-23 20:43:24 -0700

    Callback is needed

--- sensor_tripV1.py
+++ sensor_tripV1.py
@@ -35,13 +35,13 @@ Break Beam Sensor Setup
 
 BEAM_PIN = 17
 
-"""
+
 def break_beam_callback(channel):
     if GPIO.input(BEAM_PIN):
         print("beam unbroken")
     else:
         print("beam broken")
-"""
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
